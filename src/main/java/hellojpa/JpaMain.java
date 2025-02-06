4c8f1c5e0aa1dfd37bd60f91e7c6219660f8e7e9
Chaewon Jeong <jcw3241@gmail.com>
2025-02-06 23:58:17 +0900

    0206 단방향연관관계 완강

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
 
-            team.setName("Team A");
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
             tx.commit();
         } catch (Exception e) {
