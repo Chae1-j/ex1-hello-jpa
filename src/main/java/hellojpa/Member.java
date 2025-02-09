49ce635919f8a3dcf82e577f21ecade24c66150e
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2025-02-09 15:05:13 +0900

    0209 다대일, 일대다 관계 강의 실습 (결론 : 다대일 양방향을 사용하자!)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @SequenceGenerator(name="member_seq_generator",
@@ -22,9 +24,12 @@ public class Member {
 //    @Column(name="TEAM_ID")
 //    private Long teamId;
 
+//    @ManyToOne
+//    @JoinColumn(name="TEAM_ID") // 외래키가 있는 쪽이 연관관계의 주인
+
     @ManyToOne
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
+    @JoinColumn(name="TEAM_ID", insertable = false, updatable = false)
+    private Team team; // 매핑되어있고 사용은 가능하나 읽기전용이 됨(insertable, updatable을 사용함으로써).
 
     public Long getId() {
         return id;
@@ -42,11 +47,4 @@ public class Member {
         this.username = username;
     }
 
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 }
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -14,6 +14,11 @@ public class Team {
     private Long id;
     private String name;
 
+//    @OneToMany(mappedBy = "team") // 단순한 읽기만 가능해짐
+    @OneToMany
+//    @JoinColumn(name = "TEAM_ID")
+    private List<Member> members = new ArrayList<>();
+
     public List<Member> getMembers() {
         return members;
     }
@@ -21,9 +26,6 @@ public class Team {
     public void setMembers(List<Member> members) {
         this.members = members;
     }
-
-    @OneToMany(mappedBy = "team")
-    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
